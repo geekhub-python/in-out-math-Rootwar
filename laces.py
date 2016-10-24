f923272f5210b3f0e6e38a747e08a1ba0ade81fd
Bohdan Popovych <bohdan.popovych@masterofcode.com>
2016-10-24 21:57:47 +0300

    refactored laces tasks

--- laces.py
+++ laces.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-len_between_rows = int(input())
-len_between_tears = int(input())
-quantity_tears = int(input())
-free_length = int(input())
+len_between_rows = int(input('Enter length between rows: '))
+len_between_tears = int(input('Enter length between tears: '))
+free_length = int(input('Enter free length laces: '))
+quantity_tears = int(input('Enter quantity tears: '))
 
-a = len_between_rows + len_between_tears
-b = (quantity_tears * a) + free_length
-b = b * 2
+len_between_tears = len_between_tears * (quantity_tears - 1) * 2
+len_between_rows = ((len_between_rows * 2) * quantity_tears) - len_between_rows
+length_laces = len_between_rows + len_between_tears + free_length * 2
 
-print(b)
+print('Length tears: ', length_laces)
